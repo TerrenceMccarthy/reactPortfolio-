e29a6837b741c8c4646b31b5932976e0b618f108
Terrence McCarthy <terrencemccarthysnkrs@gmail.com>
2024-02-04 02:26:05 -0600

    messed with nav text color

--- react-portfolio/src/components/NavTabs.jsx
+++ react-portfolio/src/components/NavTabs.jsx
@@ -3,11 +3,9 @@ import { Link, useLocation } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import '../App.css';
 
-// issues with blurry background image for nav
-
-// issue with moving my nav tabs around with out messing up the nav drop down for smaller screens
-
+//  why is my text not changing to white? even after importing css that has !important
 
 function NavTabs() {
   const currentPage = useLocation().pathname;
@@ -15,23 +13,22 @@ function NavTabs() {
   return (
     <Navbar expand="lg" className="bg-secondary bg-gradient">
       <Container >
-        <Navbar.Brand ><img src='' /></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto" >
-            <Nav.Link >
+          <Nav className="mx-auto">
+            <Nav.Link className="white-text">
               <Link to={'/'} className={currentPage === '/' ? 'nav-link active' : 'nav-link'}>Home</Link>
             </Nav.Link>
-            <Nav.Link>
+            <Nav.Link className="white-text">
               <Link to={'/About'} className={currentPage === '/About' ? 'nav-link active' : 'nav-link'}>About Me</Link>
             </Nav.Link>
-            <Nav.Link>
+            <Nav.Link className="white-text">
               <Link to={'/Project'} className={currentPage === '/Project' ? 'nav-link active' : 'nav-link'}>Projects</Link>
             </Nav.Link>
-            <Nav.Link>
+            <Nav.Link className="white-text">
               <Link to={'/Resume'} className={currentPage === '/Resume' ? 'nav-link active' : 'nav-link'}>Resume</Link>
             </Nav.Link>
-            <Nav.Link>
+            <Nav.Link className="white-text">
               <Link to={'/Contact'} className={currentPage === '/Contact' ? 'nav-link active' : 'nav-link'}>Contact</Link>
             </Nav.Link>
           </Nav>
--- react-portfolio/src/pages/Resume.jsx
+++ react-portfolio/src/pages/Resume.jsx
@@ -1,10 +1,85 @@
-export default function About() {
-    return (
-        <div>
-            <h1>Resume Page</h1>
-            <p>
-                TEST
-            </p>
-        </div>
-    );
-}
+// Resume.jsx
+
+// Resume.jsx
+
+import React from 'react';
+import { Container, Row, Col, Card } from 'react-bootstrap';
+
+const Resume = () => {
+  return (
+    <Container className="my-5">
+      <Row>
+        <Col md={8}>
+          <Card>
+            <Card.Body>
+              <h2>Terrence McCarthy</h2>
+              <p>Email: Terrencemccarthycode@gmail.com</p>
+              <p>Phone: (682) 429-3347</p>
+            </Card.Body>
+          </Card>
+        </Col>
+
+        <Col md={4}>
+          <Card>
+            <Card.Body>
+              <h5>Skills</h5>
+              <Row>
+                <Col xs={4}>
+                  <ul>
+                    <li>JavaScript</li>
+                    <li>React</li>
+                    <li>Express</li>
+                  </ul>
+                </Col>
+
+                <Col xs={4}>
+                  <ul>
+                    <li>MongoDB</li>
+                    <li>MySQL</li>
+                    <li>Node</li>
+                  </ul>
+                </Col>
+
+                <Col xs={4}>
+                  <ul>
+                    <li>Testing</li>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                  </ul>
+                </Col>
+
+              </Row>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+
+      <Row className="mt-4">
+        <Col>
+          <Card>
+            <Card.Body>
+              <h5>Education</h5>
+              <p>Associate degree from TCC - FIND EXACT YEAR</p>
+              <p>Started Computer Science degree classes from UTA - FIND EXACT YEAR</p>
+              <p>Enrolled in SMU Full Stack coding bootcamp - Current</p>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+
+      <Row className="mt-4">
+        <Col>
+          <Card>
+            <Card.Body>
+              <h5>Experience</h5>
+              <p>No current IT experience but multiple depolyed applications on Github</p>
+              {/* Add more work experience */}
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Resume;
